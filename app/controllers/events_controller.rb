d09cf50fb71764adcbc903a19e9bd24dd852f0d7
Gregory Olive <gregoryolive@outlook.com>
2021-12-02 01:02:46 +0900

    Add show to Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[ show edit update destroy ]
+
   def index
     @events = Event.all
+  end
+
+  def show
   end
 
   def new
@@ -21,6 +26,18 @@ class EventsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: "Event was updated." }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def destroy
     @event.destroy
     respond_to do |format|
@@ -33,5 +50,9 @@ class EventsController < ApplicationController
 
   def events_params
     params.require(:event).permit(:name, :date, :location, :description)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
