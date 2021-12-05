7bad07579a2c7de87f995cdf0f462e43a8560063
Gregory Olive <gregoryolive@outlook.com>
2021-12-06 00:35:47 +0900

    Add attendee list to event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :signups, foreign_key: :attended_events_id
+  has_many :signups, foreign_key: :attended_event_id
   has_many :attendees, through: :signups, source: :event_attendee
   belongs_to :host, class_name: 'User'
 end
