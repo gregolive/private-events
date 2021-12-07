02c07d514b9aa1122fce31772073853ad5b967bd
Gregory Olive <gregoryolive@outlook.com>
2021-12-07 12:19:27 +0900

    Refactor event cards for event index and user show pages into partials

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   has_many :signups, foreign_key: :attended_event_id
   has_many :attendees, through: :signups, source: :event_attendee
   belongs_to :host, class_name: 'User'
+
+  def self.upcoming
+    return Event.all.where('date >= ?', Date.today).order(:date)
+  end
+
+  def self.past
+    return Event.all.where('date < ?', Date.today).order(:date)
+  end
 end
